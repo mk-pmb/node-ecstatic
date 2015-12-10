3732e899dcaad85eb02e8497e0cdb0edff2e8908
mk-pmb <mk-pmb@users.noreply.github.com>
2015-12-10 22:40:13 +0100

    add log messages to debug test timeout

--- test/malformed.js
+++ test/malformed.js
@@ -7,16 +7,19 @@ test('malformed uri', function (t) {
   t.plan(1);
   var server = http.createServer(ecstatic(__dirname));
   t.on('end', function () {
+    console.error('closing');
     server.close();
   });
-  
+
   server.listen(0, function () {
+    console.error('listening on ', server.address());
     var r = http.get({
       host: 'localhost',
       port: server.address().port,
       path: '/%'
     });
     r.on('response', function (res) {
+      console.error('status ', res.statusCode);
       t.equal(res.statusCode, 400);
     });
   });
